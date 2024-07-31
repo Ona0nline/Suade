687f234bd31f2b5ef446e6ad583724bc8ce8b7b3
Ona0nline <onasihle123@gmail.com>
2024-07-31 16:36:44 +0200

    updated shop

--- javascript/shop.js
+++ javascript/shop.js
@@ -6,3 +6,19 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+const product = [
+  {
+    id:0,
+    image: 'images/black front',
+    title:  'black tee',
+    price: 320
+  },
+
+  {
+    id:1,
+    image: 'images/white front.jpg',
+    title:  'white tee',
+    price: 320
+  }
+]
