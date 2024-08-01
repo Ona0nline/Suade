18fd3cefa4e6b619c4f1a445998050cebed5361d
Alexander-Agu <theonlyagu@gmail.com>
2024-08-02 00:01:00 +0200

    Got the shopping cart working

---
+++ javascript/cart.js
@@ -0,0 +1,51 @@
+// Gets Items from
+let basket = JSON.parse(localStorage.getItem('itemData')) || [];
+let cartContainer = document.getElementById('cart-container');
+
+console.log(basket)
+
+// Generates the cart item
+let generateCartItems = ()=>{
+    if(basket.length !== 0){
+        return (cartContainer.innerHTML = basket.map((x) =>{
+            let {name, price, quantity, img} = x;
+            let search = basket.find((y)=>y.name === name) || [];
+            console.log(search)
+            return `
+                <div class="cart-container">
+                    <div class="cart-item">
+                        <img width="100" src="images/black front.jpg" alt="">
+                    
+                        <div class="details">
+                            <div class="name-price">
+                            <h3 id="name">${search.name}</h3>
+                        </div>
+                    
+                        <div class="quant">
+                            <div class="price">
+                                <h3 id="price">R${search.price}</h3>
+                            </div>
+                    
+                            <div class="buttons">
+                                <p id="remove">-</p>
+                                <p id="num-items">${search.quantity}</p>
+                                <p id="add">+</p>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            
+            `
+        }).join(""))
+    } else{
+        shoppingCart.innerHTML = ``;
+        label.innerHTML = `
+        <h2> Cart is Empty</h2>
+        <a href="index.html">
+            <button class="homeBTN">Back to Home</button>
+        </a>
+        `;
+    }
+  };
+generateCartItems();
--- javascript/shop.js
+++ javascript/shop.js
@@ -46,85 +46,50 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+/*   slides[slideIndex-1].style.display = "block"; */
+ /*  */
 }
 
 /* ----- SHOPPING CART ----- */
-let shop = document.getElementById('shop');
+let addBlackItem  = document.getElementById('add-item');
+let size = document.getElementById('size');
+let quantity = document.getElementById('quantity');
 
-const storeItems = [
+
+// Everytime we select any item its going to store the data inside of the basket
+let basket = JSON.parse(localStorage.getItem('itemData')) || [];
+let numItems = Number(quantity.value)
+console.log(numItems)
+
+let storeItemsData = [
   {
-    id: 'fhgh1',
     name: 'SS1 BLACK TEE',
-    price: 320,
+    price: 320.00,
+    quantity: numItems,
     img: "images/black front.jpg"
-  },
-  {
-    id: 'fhgh2',
-    name: 'SS2 BLACK SPIDER TEE',
-    price: 200,
-    img: "images/suade black back.jpg"
-  },
-  {
-    id: 'fhgh3',
-    name: 'SS1 &SS2 BLACK TEE',
-    price: 450,
-    img: "images/black together.jpg"
-  },
-  {
-    id: 'fhgh4',
-    name: 'SS4 Bond Shirt',
-    price: 100,
-    img: "images/black pull jpg.jpg"
-  },
-];
+  }
+]
 
-// Generates the shop
-// Instead of  wriring repetetive HTML code for the add to cart section we can write it once in js along with using objects to make our work eaiser
-// I'm facing difficulties because of the way you made your shoping cart 
-let generateShop = ()=>{
-  return (shop.innerHTML = storeItems.map((x)=>{
-    let {id,name,price,img} = x;
-    return `
-  <div class="mySlides fade" id=product-id-${id}>
-    <img src="${img}" style="width:100%">
+// Adds item to cart
+addBlackItem.onclick = ()=>{
+  storeItemsData[0].quantity = quantity.value
 
-    <h2 class="name">${name}</h2>
+  // Searching for the items bane
+  let search = basket.find((x)=> x.id === basket.name);
 
-    <form id="cart-form">
-      <legend>Size</legend>
-      <select>
-        <option value="">Select a size</option>
-        <option value="xs">XS</option>
-        <option value="s">S</option>
-        <option value="m">M</option>
-        <option value="l">L</option>
-        <option value="xl">XL</option>
-      </select>
-      
-      <br/>
-
-      <div class="quantity">
-        <h3 class="price">R ${price}</h3>
-
-        <div class="add-minus">
-          <p id="minus">-</p>
-          <p id="num-items" id=${id}>0</p>
-          <p id="minus">+</p>
-        </div>
-      </div>
-
-      <div class="cart-action">
-        <button>Add to Cart</button><br/>
-        <a href="cart.html" target="_blank">View Cart</a>
-      </div>
-    </form>
-  </div>
-  `}).join(''))
+  // If its not found push the below info to basket and save it
+  if(search === undefined){
+    basket.push({
+      name: 'SS1 BLACK TEE',
+      price: 320.00 * storeItemsData[0].quantity,
+      quantity: Number(quantity.value),
+      img: "images/black front.jpg"
+    });
+  } else{ // If the item is already in the basket run the code below
+    search.quantity += 1
+    search.price += storeItemsData.price;
+  }
+  
+  // Save basket info to local storage
+  localStorage.setItem('itemData', JSON.stringify(basket));
 };
-
-generateShop()
-generateShop()
-generateShop()
-generateShop()
